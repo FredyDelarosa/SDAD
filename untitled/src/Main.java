868b6050d5b6026ff887ea883f1412abd298e92b
FredyDelarosa <fredydelarosarojascetis138@gmail.com>
2024-03-27 11:08:20 -0600

    Prueba subiendo modificaciones

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+         Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el mensaje:");
+        String msg = scanner.nextLine();
+
+        System.out.println("El mensaje es "+msg);
     }
 }
